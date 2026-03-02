1967e027b8a7c64657dd7e33da26cc3df1ce2b25
sudev-sep <sudevanss4@gmail.com>
2026-03-02 12:13:03 +0530

    update3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,10 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
+STATIC_URL = 'static/'
 
 # The VIP list of allowed frontend URLs
 CORS_ALLOWED_ORIGINS = [
@@ -163,5 +163,4 @@ REST_FRAMEWORK = {
 }
 
 # Pull the Gemini API Key securely from the environment
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-from dotenv import load_dotenv
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
